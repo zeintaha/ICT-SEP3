79fb788cf51ccc4288758543ff0bf27fa3d0d051
nadeem <266704@via.dk>
2018-10-28 08:57:24 +0100

    Main added

--- SEP3/SEP3-Code/src/dao/MainClass.java
+++ SEP3/SEP3-Code/src/dao/MainClass.java
@@ -1,5 +1,9 @@
 package dao;
 
+import model.movie.AbstractMovie;
+import model.movie.CrimeMovie;
+import model.movie.DramaMovie;
+
 public class MainClass
 {
   
@@ -10,9 +14,14 @@ public class MainClass
       final String USERNAME = "postgres";
       final String PASSWORD = "0940";
    
-   CustomerDAO cust = new CustomerDAOService(JDBC_URL, USERNAME, PASSWORD);
-   cust.create("012544", "fadi", "Horsens");
-   System.out.println(" done");
+//   CustomerDAO cust = new CustomerDAOService(JDBC_URL, USERNAME, PASSWORD);
+//   cust.create("012544", "fadi", "Horsens");
+//   System.out.println(" done");
+      
+      MovieDAO cust= new MovieDAOService(JDBC_URL, USERNAME, PASSWORD);
+      AbstractMovie movie= new CrimeMovie("Once Upon", "Nadeem","Nice movie","2 hours","www.imdb.com","www.azure.com","http://imdb.com");
+      cust.create(movie);
+      System.out.println(" done");
     
    }
 
