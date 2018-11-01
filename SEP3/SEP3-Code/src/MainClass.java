762ee574a7ecb77d1ed68d8ab819dff18b33aa59
fadi-dasus <31960592+fadi-dasus@users.noreply.github.com>
2018-11-01 11:36:41 +0100

    connection manager

--- SEP3/SEP3-Code/src/MainClass.java
+++ SEP3/SEP3-Code/src/MainClass.java
@@ -1,5 +1,5 @@
-package dao;
-
+import dao.MovieDAO;
+import dao.MovieDAOService;
 import model.movie.AbstractMovie;
 import model.movie.CrimeMovie;
 
@@ -13,7 +13,10 @@ public class MainClass
 	      final String USERNAME = "dbserveradmin@dbserversep3";
 	      final String PASSWORD = "n5*SFSVSSG&1!x";
    
-
+//   CustomerDAO cust = new CustomerDAOService(JDBC_URL, USERNAME, PASSWORD);
+//   cust.create("012544", "fadi", "Horsens");
+//   System.out.println(" done");
+      
       MovieDAO cust= new MovieDAOService(JDBC_URL, USERNAME, PASSWORD);
       AbstractMovie movie= new CrimeMovie("Once Upon Fadi version", "Nadeem","Nice movie","2 hours","www.imdb.com","www.azure.com","http://imdb.com");
       cust.create(movie);
--- SEP3/SEP3-Code/src/dao/TestTheDatabaseHelper.java
+++ SEP3/SEP3-Code/src/dao/TestTheDatabaseHelper.java
@@ -12,28 +12,26 @@ public class TestTheDatabaseHelper
    public static void main(String[] args) throws RemoteException, SQLException
    {
 
-      
-      
+      // Create a variable for the connection string.
+      String connectionUrl = "jdbc:sqlserver://dbserversep3.database.windows.net:1433;database=moviedb;user=dbserveradmin@dbserversep3;password=n5*SFSVSSG&1!x;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
+      try (Connection con = DriverManager.getConnection(connectionUrl);
+            Statement stmt = con.createStatement();)
+      {
+         String SQL = "select name from Customer";
+         ResultSet rs = stmt.executeQuery(SQL);
 
-          // Create a variable for the connection string.
-          String connectionUrl = "jdbc:sqlserver://dbserversep3.database.windows.net:1433;database=moviedb;user=dbserveradmin@dbserversep3;password=n5*SFSVSSG&1!x;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-
-          try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
-              String SQL = "select name from Customer";
-              ResultSet rs = stmt.executeQuery(SQL);
-
-              // Iterate through the data in the result set and display it.
-              while (rs.next()) {
-                  System.out.println(rs.getString("name"));
-              }
-          }
-          // Handle any errors that may have occurred.
-          catch (SQLException e) {
-              e.printStackTrace();
-          }
+         // Iterate through the data in the result set and display it.
+         while (rs.next())
+         {
+            System.out.println(rs.getString("name"));
+         }
       }
-     
-   
+      // Handle any errors that may have occurred.
+      catch (SQLException e)
+      {
+         e.printStackTrace();
+      }
+   }
 
 }
