db1321849f2d5d36059b436bbb9945026a78eaa8
Taha <zeintaha@hotmail.com>
2018-12-12 16:20:15 +0100

    add client admin

---
+++ SEP3/MovieWorldAdmin/MovieWorld/FormLogin.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieWorld
+{
+    public partial class FormLogin : Form
+    {
+        private bool _dragging;
+        private Point _offset;
+
+        public Form RefToMain { get; set; }
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        Login login = new Login("Taha", "1234");
+
+        private void FormLogin_MouseDown(object sender, MouseEventArgs e)
+        {
+            _offset.X = e.X;
+            _offset.Y = e.Y;
+            _dragging = true;
+        }
+
+        private void FormLogin_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point currentScreenPos = PointToScreen(e.Location);
+                Location = new Point
+                    (currentScreenPos.X - _offset.X,
+                     currentScreenPos.Y - _offset.Y);
+            }
+        }
+
+        private void FormLogin_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            this.RefToMain.Show();
+            Close();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            //define local variables from the user inputs 
+            string user = textBox_Username.Text;
+            string pass = textBox_Password.Text;
+            //check if eligible to be logged in 
+            if (login.IsLoggedIn(user, pass))
+            {
+                //MessageBox.Show("You are logged in successfully");
+                User userfrm = new User();
+                login.Isuser = true;
+                if (login.Isuser == true) {
+                    Session.IsSession = true;
+
+                }
+                else if (login.Isuser == false)
+                {
+                    Session.IsSession = true;
+
+                }
+
+                userfrm.Show();
+                this.Hide();
+            }
+            else
+            {
+                //show default login error message 
+                MessageBox.Show("Login Error!");
+            }
+        }
+
+
+    }
+}
