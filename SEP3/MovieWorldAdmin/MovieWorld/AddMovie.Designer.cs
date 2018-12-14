f67a0f267758856c4063179ff3db16cef625e56e
Balkis Ibrahim <43548806+Balqies@users.noreply.github.com>
2018-12-14 12:03:44 +0100

    2
    burndown

--- SEP3/MovieWorldAdmin/MovieWorld/AddMovie.Designer.cs
+++ SEP3/MovieWorldAdmin/MovieWorld/AddMovie.Designer.cs
@@ -51,54 +51,60 @@
             // 
             this.textBox_MovieURL.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_MovieURL.ForeColor = System.Drawing.Color.Black;
-            this.textBox_MovieURL.Location = new System.Drawing.Point(365, 409);
+            this.textBox_MovieURL.Location = new System.Drawing.Point(274, 332);
+            this.textBox_MovieURL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_MovieURL.Name = "textBox_MovieURL";
-            this.textBox_MovieURL.Size = new System.Drawing.Size(365, 30);
+            this.textBox_MovieURL.Size = new System.Drawing.Size(275, 25);
             this.textBox_MovieURL.TabIndex = 8;
             // 
             // textBox_Description
             // 
             this.textBox_Description.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_Description.ForeColor = System.Drawing.Color.Black;
-            this.textBox_Description.Location = new System.Drawing.Point(365, 139);
+            this.textBox_Description.Location = new System.Drawing.Point(274, 113);
+            this.textBox_Description.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_Description.Name = "textBox_Description";
-            this.textBox_Description.Size = new System.Drawing.Size(365, 30);
+            this.textBox_Description.Size = new System.Drawing.Size(275, 25);
             this.textBox_Description.TabIndex = 3;
             // 
             // textBox_Duration
             // 
             this.textBox_Duration.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_Duration.ForeColor = System.Drawing.Color.Black;
-            this.textBox_Duration.Location = new System.Drawing.Point(365, 247);
+            this.textBox_Duration.Location = new System.Drawing.Point(274, 201);
+            this.textBox_Duration.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_Duration.Name = "textBox_Duration";
-            this.textBox_Duration.Size = new System.Drawing.Size(365, 30);
+            this.textBox_Duration.Size = new System.Drawing.Size(275, 25);
             this.textBox_Duration.TabIndex = 5;
             // 
             // textBox_ImageURL
             // 
             this.textBox_ImageURL.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_ImageURL.ForeColor = System.Drawing.Color.Black;
-            this.textBox_ImageURL.Location = new System.Drawing.Point(365, 301);
+            this.textBox_ImageURL.Location = new System.Drawing.Point(274, 245);
+            this.textBox_ImageURL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_ImageURL.Name = "textBox_ImageURL";
-            this.textBox_ImageURL.Size = new System.Drawing.Size(365, 30);
+            this.textBox_ImageURL.Size = new System.Drawing.Size(275, 25);
             this.textBox_ImageURL.TabIndex = 6;
             // 
             // textBox_TrailerURL
             // 
             this.textBox_TrailerURL.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_TrailerURL.ForeColor = System.Drawing.Color.Black;
-            this.textBox_TrailerURL.Location = new System.Drawing.Point(365, 355);
+            this.textBox_TrailerURL.Location = new System.Drawing.Point(274, 288);
+            this.textBox_TrailerURL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_TrailerURL.Name = "textBox_TrailerURL";
-            this.textBox_TrailerURL.Size = new System.Drawing.Size(365, 30);
+            this.textBox_TrailerURL.Size = new System.Drawing.Size(275, 25);
             this.textBox_TrailerURL.TabIndex = 7;
             // 
             // textBox_Title
             // 
             this.textBox_Title.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_Title.ForeColor = System.Drawing.Color.Black;
-            this.textBox_Title.Location = new System.Drawing.Point(365, 32);
+            this.textBox_Title.Location = new System.Drawing.Point(274, 26);
+            this.textBox_Title.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_Title.Name = "textBox_Title";
-            this.textBox_Title.Size = new System.Drawing.Size(365, 30);
+            this.textBox_Title.Size = new System.Drawing.Size(275, 25);
             this.textBox_Title.TabIndex = 1;
             // 
             // button_Save
@@ -106,9 +112,10 @@
             this.button_Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_Save.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_Save.ForeColor = System.Drawing.Color.White;
-            this.button_Save.Location = new System.Drawing.Point(451, 462);
+            this.button_Save.Location = new System.Drawing.Point(338, 375);
+            this.button_Save.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_Save.Name = "button_Save";
-            this.button_Save.Size = new System.Drawing.Size(150, 42);
+            this.button_Save.Size = new System.Drawing.Size(112, 34);
             this.button_Save.TabIndex = 9;
             this.button_Save.Text = "Save";
             this.button_Save.UseVisualStyleBackColor = true;
@@ -126,9 +133,10 @@
             "Crime",
             "Drama",
             "Horror"});
-            this.comboBox_Genre.Location = new System.Drawing.Point(365, 86);
+            this.comboBox_Genre.Location = new System.Drawing.Point(274, 70);
+            this.comboBox_Genre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox_Genre.Name = "comboBox_Genre";
-            this.comboBox_Genre.Size = new System.Drawing.Size(365, 29);
+            this.comboBox_Genre.Size = new System.Drawing.Size(275, 27);
             this.comboBox_Genre.TabIndex = 2;
             this.comboBox_Genre.Text = "Select Genres";
             // 
@@ -137,9 +145,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(234, 410);
+            this.label2.Location = new System.Drawing.Point(176, 333);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 22);
+            this.label2.Size = new System.Drawing.Size(88, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "Movie URL";
             // 
@@ -148,21 +157,22 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(206, 86);
+            this.label3.Location = new System.Drawing.Point(154, 70);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(136, 22);
+            this.label3.Size = new System.Drawing.Size(111, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Select Genres";
-            
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(231, 140);
+            this.label4.Location = new System.Drawing.Point(173, 114);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(111, 22);
+            this.label4.Size = new System.Drawing.Size(91, 20);
             this.label4.TabIndex = 6;
             this.label4.Text = "Description";
             // 
@@ -171,9 +181,10 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(253, 248);
+            this.label5.Location = new System.Drawing.Point(190, 202);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(89, 22);
+            this.label5.Size = new System.Drawing.Size(71, 20);
             this.label5.TabIndex = 6;
             this.label5.Text = "Duration";
             // 
@@ -182,9 +193,10 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label6.Location = new System.Drawing.Point(234, 302);
+            this.label6.Location = new System.Drawing.Point(176, 245);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(108, 22);
+            this.label6.Size = new System.Drawing.Size(88, 20);
             this.label6.TabIndex = 6;
             this.label6.Text = "Image URL";
             // 
@@ -193,9 +205,10 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label7.Location = new System.Drawing.Point(243, 356);
+            this.label7.Location = new System.Drawing.Point(182, 289);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(99, 22);
+            this.label7.Size = new System.Drawing.Size(81, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Trailer URL";
             // 
@@ -204,9 +217,10 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label8.Location = new System.Drawing.Point(231, 32);
+            this.label8.Location = new System.Drawing.Point(173, 26);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(105, 22);
+            this.label8.Size = new System.Drawing.Size(87, 20);
             this.label8.TabIndex = 6;
             this.label8.Text = "Movie Title";
             // 
@@ -215,9 +229,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(231, 194);
+            this.label1.Location = new System.Drawing.Point(173, 158);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 22);
+            this.label1.Size = new System.Drawing.Size(68, 20);
             this.label1.TabIndex = 10;
             this.label1.Text = "Director";
             // 
@@ -225,18 +240,19 @@
             // 
             this.textBox_Director.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_Director.ForeColor = System.Drawing.Color.Black;
-            this.textBox_Director.Location = new System.Drawing.Point(365, 193);
+            this.textBox_Director.Location = new System.Drawing.Point(274, 157);
+            this.textBox_Director.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_Director.Name = "textBox_Director";
-            this.textBox_Director.Size = new System.Drawing.Size(365, 30);
+            this.textBox_Director.Size = new System.Drawing.Size(275, 25);
             this.textBox_Director.TabIndex = 4;
             // 
             // AddMovie
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.CausesValidation = false;
-            this.ClientSize = new System.Drawing.Size(964, 556);
+            this.ClientSize = new System.Drawing.Size(723, 452);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox_Director);
             this.Controls.Add(this.label8);
@@ -257,12 +273,11 @@
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AddMovie";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddMovie";
-            
-            
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.AddMovie_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AddMovie_MouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.AddMovie_MouseUp);
