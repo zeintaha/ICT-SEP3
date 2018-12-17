933549946ff6401a2cb78c51529ce50ca29bc695
Taha <zeintaha@hotmail.com>
2018-12-17 23:07:29 +0100

    Update ReadMessage.Designer.cs

--- SEP3/MovieWorldAdmin/MovieWorld/ReadMessage.Designer.cs
+++ SEP3/MovieWorldAdmin/MovieWorld/ReadMessage.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ReadMessage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ReadMessage";
             this.Text = "ReadMessage";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
