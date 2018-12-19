34686840040144ebbd853d805090a08728b19d3d
fadi-dasus <31960592+fadi-dasus@users.noreply.github.com>
2018-12-19 01:15:21 +0100

    feedback

---
+++ SEP3/sep3JPA/src/main/java/sep/via/dk/sep3JPA/dao/feedBack/FeedBackImplementation.java
@@ -0,0 +1,35 @@
+package sep.via.dk.sep3JPA.dao.feedBack;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import sep.via.dk.sep3JPA.domain.FeedBack;
+import sep.via.dk.sep3JPA.domain.movie.Movie;
+
+@Transactional
+@Repository
+public class FeedBackImplementation implements FeedBackDAO {
+	@PersistenceContext
+	@Autowired
+	public EntityManager entityManager;
+
+	@Override
+	public void addFeedBack(FeedBack feedBack) {
+		entityManager.persist(feedBack);
+
+	}
+
+	@Override
+	public List<FeedBack> getAllFeedBack() {
+		String query = "select m from FeedBack m order by m.username";
+
+		return (List<FeedBack>) entityManager.createQuery(query).getResultList();
+	}
+
+}
--- SEP3/sep3JPA/src/main/java/sep/via/dk/sep3JPA/socket/Message.java
+++ SEP3/sep3JPA/src/main/java/sep/via/dk/sep3JPA/socket/Message.java
@@ -2,22 +2,35 @@ package sep.via.dk.sep3JPA.socket;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Message")
 public class Message implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "message_id")
+	private Integer id;
+
+	@Column(name = "message")
 	private String message;
-	
-	
+
 	public Message() {
-		
+
 	}
 
-public Message(String message) {
-		
+	public Message(String message) {
+		this.message = message;
 	}
 
 	public String getMessage() {
 		return message;
 	}
-
 
 	public void setMessage(String message) {
 		this.message = message;
